cb5b47cba68fc033a17bb667be2be1f0e95a2e0d
Tommy Lin <tommy.lin@rotman.utoronto.ca>
2022-08-08 20:10:06 -0300

    final commit before pushing onto github

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux"
+import { store } from "./state/store"
+import RepositoriesList from './RepositoriesList'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div>
+        <h1>Search for a package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
